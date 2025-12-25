c9e0e6f3fa2bca94884a4b77a626b53461bb0b46
takayuki <jesusanduchimura@gmail.com>
2025-12-25 11:32:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const characters = [
   { name: "トモ 169cm", img: "images/tomo.png", x: 1200 },
   { name: "新人ちゃん 168cm", img: "images/sinjin.png", x: 400 },
   { name: "ローランド 168cm", img: "images/roland.png", x: 730 },
-  { name: "ヒロヤ 167cm", img: "images/hiroya.png", x: 2180 },
-  { name: "カズヤ 167cm", img: "images/kazuya.png", x: 2100 },
+  { name: "ヒロヤ 167cm", img: "images/hiroya.png", x: 2220 },
+  { name: "カズヤ 167cm", img: "images/kazuya.png", x: 2320 },
   { name: "チャン 167cm", img: "images/chan.png", x: 1100 },
   { name: "アンドル 163cm", img: "images/andol.png", x: 600 },
-  { name: "店長 153cm", img: "images/manager.png", x: 2300 },
+  { name: "店長 153cm", img: "images/manager.png", x: 2400 },
   { name: "ミリィ 149cm", img: "images/milly.png", x: 2050 },
   { name: "アルク 110cm", img: "images/alc.png", x: 2200 }
 
